4bccb336066c67f41ffd566dfaa9ce24ae8f9987
haledalolabi <haledalolabi@gmail.com>
2025-08-24 22:50:04 +0300

    feat: wire up auth and admin

--- auth.ts
+++ auth.ts
@@ -2,31 +2,26 @@ import NextAuth, { type NextAuthConfig } from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import EmailProvider from "next-auth/providers/nodemailer";
 import { prisma } from "@/lib/prisma";
-import type { JWT } from "next-auth/jwt";
 import type { User } from "@prisma/client";
+import type { JWT } from "next-auth/jwt";
 
 export const authConfig: NextAuthConfig = {
   adapter: PrismaAdapter(prisma),
   session: { strategy: "jwt" },
-  providers:
-    process.env.EMAIL_SERVER && process.env.EMAIL_FROM
-      ? [
-          EmailProvider({
-            server: process.env.EMAIL_SERVER,
-            from: process.env.EMAIL_FROM,
-            maxAge: 10 * 60,
-          }),
-        ]
-      : [],
+  providers: [
+    EmailProvider({
+      server: process.env.EMAIL_SERVER!,
+      from: process.env.EMAIL_FROM!,
+      maxAge: 10 * 60,
+    }),
+  ],
   callbacks: {
     async jwt({ token, user }) {
-      if (user) (token as JWT).role = (user as User).role ?? "USER";
+      if (user) token.role = (user as User).role ?? "USER";
       return token;
     },
     async session({ session, token }) {
-      if (session.user)
-        (session.user as { role?: string }).role =
-          (token as JWT).role ?? "USER";
+      if (session.user) session.user.role = (token as JWT).role ?? "USER";
       return session;
     },
   },
@@ -34,9 +29,4 @@ export const authConfig: NextAuthConfig = {
   secret: process.env.AUTH_SECRET,
 };
 
-export const {
-  handlers: { GET, POST },
-  auth,
-  signIn,
-  signOut,
-} = NextAuth(authConfig);
+export const { handlers, auth, signIn, signOut } = NextAuth(authConfig);
--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,12 @@
 import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
-import { getToken } from "next-auth/jwt";
+import { auth } from "@/auth";
 
 export async function middleware(req: NextRequest) {
   const url = req.nextUrl;
   if (url.pathname.startsWith("/admin")) {
-    const token = await getToken({ req });
-    const role = (token as { role?: string } | null)?.role;
+    const session = await auth();
+    const role = session?.user.role;
     if (!role || (role !== "ADMIN" && role !== "EDITOR")) {
       const signInUrl = new URL("/auth/signin", url);
       return NextResponse.redirect(signInUrl);
